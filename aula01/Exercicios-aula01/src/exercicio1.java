0f1bb0e23a19362a0ab48263d2ce9fed5ae0d41a
Denison Barcelos Feijó <denibarcelosdenison@hotmail.com>
2023-10-21 20:22:41 -0300

    atualizaçao dos códigos de aula.

--- aula01/Exercicios-aula01/src/exercicio1.java
+++ aula01/Exercicios-aula01/src/exercicio1.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
 public class exercicio1 {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o seu nome");
+        String nome = scanner.nextLine();
+        System.out.println("O nome do usuario é "+ nome );
+
+    }
 }
