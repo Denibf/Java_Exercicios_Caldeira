ca9810444efdb218770c61b59667339853e4fc25
Denison Barcelos Feijó <denibarcelosdenison@hotmail.com>
2023-10-22 16:38:48 -0300

    atualizaçao dos códigos de aula.

--- aula01/Exercicios-aula01/src/exercicio3.java
+++ aula01/Exercicios-aula01/src/exercicio3.java
@@ -3,20 +3,52 @@
 import java.util.Scanner;
 public class exercicio3 {
     public static void main (String[]args){
+            double numero1, numero2, numero3,menornum, maiornum, media;
 
             Scanner scanner = new Scanner(System.in);
             System.out.println("Digite o primeiro número");
-            String numero1 = scanner.nextLine();
+            numero1 = scanner.nextDouble();
             System.out.println(numero1);
             System.out.println("Digite o segundo número");
-            String numero2 =scanner.nextLine();
+            numero2 =scanner.nextDouble();
             System.out.println(numero2);
             System.out.println("Digite o teeceiro número");
-            String numero3 = scanner.nextLine();
+            numero3 = scanner.nextDouble();
             System.out.println(numero3);
 
+            //maior numero
+            if (numero1 > numero2) {
+                    if (numero1 > numero3) {
+                            maiornum = numero1;
+                    } else {
+                            maiornum = numero3;
+                    }
+            } else {
+                    if (numero2 > numero3){
+                            maiornum = numero2;
+                    }else{
+                            maiornum = numero3;
+                    }
+            }
+            //menor numero
+            if (numero1<numero2){
+                    if(numero1<numero3){
+                    menornum = numero1;
+                    }else {
+                            menornum = numero3;
+                    }
+            }else {
+                    if (numero2<numero3){
+                            menornum = numero2;
+                    }else {
+                            menornum = numero3;
+                    }
+            }
 
+            media = (numero1+ numero2+numero3)/3.0;
 
-
+            System.out.println("O maior numero é: " + maiornum);
+            System.out.println("O menor número é: " +menornum);
+            System.out.println("A médria aritimetica é: " + media);
     }
 }
