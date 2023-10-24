238caadbf7fef2bccabdd347dd95d998deabc39f
Denison Barcelos Feijó <denibarcelosdenison@hotmail.com>
2023-10-24 01:27:44 -0300

    atualizaçao dos códigos de aula.

--- aula01/Exercicios-aula01/src/exercicio11.java
+++ aula01/Exercicios-aula01/src/exercicio11.java
@@ -1,2 +1,26 @@
+//Peça ao usuário que digite uma palavra ou frase e conte quantas vogais (a, e, i, o, u) estão
+// presentes. Exiba o número de vogais na tela.
+
+import java.util.Locale;
+import java.util.Scanner;
 public class exercicio11 {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite uma palavra ou frase");
+        String entrada = scanner.nextLine().toLowerCase();
+
+        int vogais = 0;
+
+        for (int i = 0; i < entrada.length(); i++ );
+        char letras = entrada.charAt(i);
+
+        if (letras == 'a' || letras == 'e' || letras == 'i' || letras == 'o' || letras == 'u'){
+            vogais ++;
+        }
+
+
+        System.out.println("As quantidade de vogais que tem é: " + vogais);
+
+
+    }
 }
